daeab489b13bea743bd67a86477ba2c4ef248260
bravit <bravit111@gmail.com>
2019-11-03 17:34:10 +0300

    add runner

---
+++ runAll.sh
@@ -0,0 +1,8 @@
+node --experimental-modules js-for-tcs-01.mjs
+node --experimental-modules js-for-tcs-02.mjs
+node --experimental-modules js-for-tcs-03.mjs
+node --experimental-modules js-for-tcs-04.mjs
+node --experimental-modules js-for-tcs-04a.mjs
+node --experimental-modules js-for-tcs-05.mjs
+node --experimental-modules js-for-tcs-06.mjs
+
